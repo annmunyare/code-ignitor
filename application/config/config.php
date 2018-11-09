fdf0f448c995ab649665e822eb49d0b154230610
annmunyare <ann.munyare@nanyukiappfactory.onmicrosoft.com>
2018-11-09 16:12:18 +0300

    n

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://nanyukiaf-ann-mawinguapi-debug.scm.azurewebsites.net/';
+$config['base_url'] = 'https://nanyukiaf-ann-mawingu-api-linux.azurewebsites.net/';
 // $config['base_url'] = 'http://localhost/mawingu/mawingu_api/';
 
 /*
